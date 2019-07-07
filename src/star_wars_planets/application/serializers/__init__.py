0ab2561806a3a0dba6acb29e51758bf9c8d01532
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-07 19:28:20 -0300

    Create 'parse_request_body' decorator

---
+++ src/star_wars_planets/application/serializers/__init__.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+from sanic.response import json
+from marshmallow import ValidationError
+
+
+def parse_request_body(serializer):
+    def decorator(func):
+        @wraps(func)
+        async def decorated_function(_self, request, *args, **kwargs):
+            try:
+                kwargs['body'] = serializer.Schema(strict=True).load(request.json or {}).data
+            except ValidationError as err:
+                return json(body=err.messages, status=400)
+            
+            return await func(_self, request, *args, **kwargs)
+        return decorated_function
+    return decorator
