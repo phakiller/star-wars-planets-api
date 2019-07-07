e29ae9f814edf9caad621bef4b03194e4d94a6d4
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-07 19:48:14 -0300

    Create method, 'get', on 'PlanetsResource' to expose HTTP Verb, GET, on route '/planets'

--- src/star_wars_planets/adapters/rest/planet/planet.py
+++ src/star_wars_planets/adapters/rest/planet/planet.py
@@ -16,3 +16,8 @@ class PlanetsResource(Resource):
             await self.planet_application.save_planet(body),
             status=201,
         )
+    
+    async def get(self, request) -> HTTPResponse:
+        return json(
+            await self.planet_application.get_planets()
+        )
