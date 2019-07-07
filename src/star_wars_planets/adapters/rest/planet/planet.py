65f26bb41ceb7dcf82db1f26de8c1af22ed1b6fd
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-07 20:36:41 -0300

    Modify 'get' method to handle queries string and pass 'name' args if it is passed on a request

--- src/star_wars_planets/adapters/rest/planet/planet.py
+++ src/star_wars_planets/adapters/rest/planet/planet.py
@@ -2,7 +2,7 @@ from sanic_restful import Resource
 from sanic.response import HTTPResponse, json
 
 from star_wars_planets.application.planet.planet_application import PlanetApplication
-from star_wars_planets.application.serializers import parse_request_body
+from star_wars_planets.application.serializers import parse_request_body, parse_queries_string
 from star_wars_planets.application.serializers.planet import PlanetSerializer
 
 
@@ -17,7 +17,8 @@ class PlanetsResource(Resource):
             status=201,
         )
     
-    async def get(self, request) -> HTTPResponse:
+    @parse_queries_string
+    async def get(self, request, args: dict = None) -> HTTPResponse:
         return json(
-            await self.planet_application.get_planets()
+            await self.planet_application.get_planets(args.get('name'))
         )
