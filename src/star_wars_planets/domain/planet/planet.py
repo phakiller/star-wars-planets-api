94e391f94914447d6be4c43386258c71f87c07c8
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-07 19:25:00 -0300

    Create 'Planet' domain class

---
+++ src/star_wars_planets/domain/planet/planet.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from sanic_motor import BaseModel
+
+
+class Planet(BaseModel):
+    __coll__ = 'planets'
+    __unique_fields__ = ['_id',]
+
+    _id: str
+    name: str
+    climate: str
+    terrain: str
+    number_of_movies: int
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'climate': self.climate,
+            'terrain': self.terrain,
+            'number_of_movies': self.number_of_movies,
+        }
+
+    async def save(self) -> Planet:
+        res = await self.insert_one(self.to_dict())
+        self._id = res.inserted_id
+        return self
