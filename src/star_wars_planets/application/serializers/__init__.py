eba221ed8c26030e71a38341a4f3824052a83d59
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-07 20:32:50 -0300

    Create decorator 'parse_queries_string'

--- src/star_wars_planets/application/serializers/__init__.py
+++ src/star_wars_planets/application/serializers/__init__.py
@@ -16,3 +16,11 @@ def parse_request_body(serializer):
             return await func(_self, request, *args, **kwargs)
         return decorated_function
     return decorator
+
+
+def parse_queries_string(func):
+    @wraps(func)
+    async def decorated_function(_self, request, *args, **kwargs):
+        kwargs['args'] = dict(request.query_args)
+        return await func(_self, request, *args, **kwargs)
+    return decorated_function
